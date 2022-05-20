b73649010f2a310deb9345d9bf058bc8d9016415
spryzor <105906391+spryzor@users.noreply.github.com>
2022-05-20 08:37:29 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,7 +8,6 @@ function vitesse () {
 }
 function affvit () {
     I2C_LCD1602.clear()
-    I2C_LCD1602.ShowNumber(mes, 0, 1)
     v = mes * 1.875 / 6
     I2C_LCD1602.ShowNumber(v, 0, 0)
     if (v < 10) {
@@ -17,7 +16,11 @@ function affvit () {
     if (v >= 10) {
         I2C_LCD1602.ShowString("km/h", 4, 0)
     }
+    d = v / (3.6 * 1)
+    I2C_LCD1602.ShowNumber(d, 0, 1)
+    I2C_LCD1602.ShowString("m", 5, 5)
 }
+let d = 0
 let v = 0
 let mes = 0
 let n = 0
